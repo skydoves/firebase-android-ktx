65f707c44d5d1ae0edeb5ccd020517c62a5ffc45
skydoves <skydoves@naver.com>
2024-09-03 14:21:25 +0900

    Reset version code

--- buildSrc/src/main/kotlin/com/skydoves/firebase/android/ktx/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/firebase/android/ktx/Configuration.kt
@@ -8,7 +8,7 @@ object Configuration {
   const val minorVersion = 1
   const val patchVersion = 0
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 6
+  const val versionCode = 1
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
